a247009b89c9dff7e6b41f3283ae4bf01e651471
Gifftybabe <ulokangozi@gmail.com>
2022-11-04 22:05:16 +0100

    Updated

--- app/Http/Controllers/ArithmeticController.php
+++ app/Http/Controllers/ArithmeticController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class ArithmeticController extends Controller
 {
-    //
+   public function calculate(Request $request)
+    {
+        $operation_type = $request->operation_type;
+        $x = $request->x;
+        $y = $request->y;
+
+        switch($operation_type) {
+            case 'addition':
+                $result = $x + $y;
+            break;
+            case 'subtraction':
+                $result = $x - $y;
+            break;
+             case 'multiplication':
+                $result = $x * $y;
+            break;
+        }
+
+        return response()->json([
+            'slackUsername' => 'Gifftybabe',
+            'operation_type' => $operation_type,
+            'result' => $result
+        ]);
+
+    }
 }
